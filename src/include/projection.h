f069f52e3e3d219f9e86868c0993ff1cee60c339
acse-47adf1eb <rd422@ic.ac.uk>
2023-03-20 11:37:38 +0000

    fix;

--- src/include/projection.h
+++ src/include/projection.h
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 # pragma once
+=======
+# pragma once
+# include <string>
+# include <vector>
+# include "image.h"
+
+vector update(Image img, vector current);
+>>>>>>> a00a413 (temp)
