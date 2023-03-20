51a1832e05561bfc3f3b55b8a66953e0aadb379f
chunyang.w <cw1722@ic.ac.uk>
2023-03-20 11:20:40 +0000

    add volumn implementation

--- src/include/volume.h
+++ src/include/volume.h
@@ -1,6 +1,13 @@
+# include <vector>
+# include <string>
 # include "image.h"
 
-class Volumn {
+class Volume {
     public:
+        int total_img;
+        int img_num;
 
+        std::vector<std::string> files;
+        std::string root_dir;
+        Volume(std::string root_dir);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,26 @@
 # include <vector>
 # include "image.h"
 # include "filter.h"
+# include "volume.h"
 
 using namespace std;
 
 int main() {
-    Image img("../Images/gracehopper.png");
-    Filter box3x3("./filter/box3x3.txt");
-    Filter gaussian3x3("./filter/gaussian3x3.txt");
-    Filter box5x5("./filter/box5x5.txt");
-    Filter sobelx("./filter/sobelx.txt");
-    Filter prewittx("./filter/prewittx.txt");
+    Volume voxel("../Scans/confuciusornis/");
+    // Image img("../Images/gracehopper.png");
+    // Filter box3x3("./filter/box3x3.txt");
+    // Filter gaussian3x3("./filter/gaussian3x3.txt");
+    // Filter box5x5("./filter/box5x5.txt");
+    // Filter sobelx("./filter/sobelx.txt");
+    // Filter prewittx("./filter/prewittx.txt");
 
-    box3x3.apply(img).write("../Output/box_gracehopper.png");
-    gaussian3x3.apply(img).write("../Output/gau_gracehopper.png");
-    box5x5.apply(img).write("../Output/box5_gracehopper.png");
-    sobelx.apply(box3x3.apply(grayScale(img))).write("../Output/sobelx_gracehopper.png");
-    prewittx.apply(box3x3.apply(grayScale(img))).write("../Output/prewittx_gracehopper.png");
+    // box3x3.apply(img).write("../Output/box_gracehopper.png");
+    // gaussian3x3.apply(img).write("../Output/gau_gracehopper.png");
+    // box5x5.apply(img).write("../Output/box5_gracehopper.png");
+    // sobelx.apply(box3x3.apply(grayScale(img))).write("../Output/sobelx_gracehopper.png");
+    // prewittx.apply(box3x3.apply(grayScale(img))).write("../Output/prewittx_gracehopper.png");
 
-    sobel(grayScale(img)).write("../Output/sobel_gracehopper.png");
-    prewitt(grayScale(img)).write("../Output/prewitt_gracehopper.png");
+    // sobel(grayScale(img)).write("../Output/sobel_gracehopper.png");
+    // prewitt(grayScale(img)).write("../Output/prewitt_gracehopper.png");
     return 0;
 }
