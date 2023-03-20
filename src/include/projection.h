c56db01f5fce0d2d3430e5406bdffc1ba17e9240
chunyang.w <cw1722@ic.ac.uk>
2023-03-20 14:24:14 +0000

    merge;

--- src/include/projection.h
+++ src/include/projection.h
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-# pragma once
-=======
 # pragma once
 # include <string>
 # include <vector>
 # include "image.h"
-
-vector update(Image img, vector current);
->>>>>>> a00a413 (temp)
