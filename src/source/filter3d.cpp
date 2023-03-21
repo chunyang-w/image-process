700209b8b04327d67ce52768f60d3259f19581e0
acse-47adf1eb <rd422@ic.ac.uk>
2023-03-21 17:43:43 +0000

    filter modified

--- src/source/filter3d.cpp
+++ src/source/filter3d.cpp
@@ -9,6 +9,8 @@
 # include "fastimage.h"
 # include <cmath>
 # include <map>
+# include <algorithm>
+
 
 
 using namespace std;
@@ -37,7 +39,7 @@ using namespace std;
 //     {{0.0206, 0.0339, 0.0206}, {0.0339, 0.0560, 0.0339}, {0.0206, 0.0339, 0.0206}}
 // };
 FImage filter_3d(Volume voxel){
-    int kernel_size = 5;
+    int kernel_size = 3;
     double res = 0;
     int photo_num = 1;
     double kernel[kernel_size][kernel_size][kernel_size];
@@ -70,18 +72,15 @@ FImage filter_3d(Volume voxel){
     }
     int padding = (kernel_size-1)/2;
     FImage res_img(voxel.files[0]);
-    // int w = res_img.width; int h = res_img.height; int c = res_img.channel;
-    for (int m = padding; m < voxel.img_num - padding; m++) {
+    cout<< voxel.img_num << endl;
+    // for (int o = kernel_size-1; o < voxel.img_num; o++) {
+    for (int o = kernel_size-1; o < 10; o++) {
+        cout << o << endl;
+        if(o != kernel_size-1){
         buffer.pop_front();
-        FImage single_img(voxel.files[m]);
+        FImage single_img(voxel.files[o]);
         buffer.push_back(single_img);
-        // for (int i = m - padding; i < m + padding; i++) {
-        //     read_figure.erase();
-        //     read_figure.push_back(FImage(voxel.files[i]));
-        // }
-        // FImage left(voxel.files[m-1]);
-        // FImage middle(voxel.files[m]);
-        // FImage right(voxel.files[m+1]);
+        }
         for (int i = padding; i < res_img.height-padding; i++) {
             for (int j = padding; j < res_img.width-padding; j++) {
                 for (int k = 0; k < res_img.channel; k++) {
@@ -89,15 +88,6 @@ FImage filter_3d(Volume voxel){
                         for(int n=0; n<kernel_size; n++){
                             for(int p=0; p<kernel_size; p++){
                                 res += kernel[m][n][p] * buffer[m].getPixel(i+n-padding, j+p-padding, k);
-                                // if (m == 0){
-                                //     res += kernel[m][n][p] * left.getPixel(i+n-padding, j+p-padding, k);
-                                // }
-                                // else if (m == 1){
-                                //     res += kernel[m][n][p] * middle.getPixel(i+n-padding, j+p-padding, k);
-                                // }
-                                // else if (m == 2){
-                                //      res += kernel[m][n][p] * right.getPixel(i+n-padding, j+p-padding, k);
-                                // }
                             }
                         }
                     }
@@ -110,16 +100,10 @@ FImage filter_3d(Volume voxel){
         string path = root_path + to_string(photo_num) + ".png";
         res_img.write(path);
     }
+    cout << "here" << endl;
+    return res_img;
 }
-# include <deque>
-# include <string>
-# include <iostream>
-# include <algorithm>
-# include "volume.h"
-# include "fastImage.h"
-# include "filter3d.h"
 
-using namespace std;
 
 FImage getMedian(deque<FImage> &buffer, int kernel_size, int h, int w, int c) {
     int padding = (kernel_size - 1) / 2;
@@ -171,42 +155,3 @@ Volume median3d(Volume voxel, int kernel_size) {
     }
     return voxel;
 }
-
-// Volume median3d(Volume voxel, int kernel_size) {
-//     cout << "in gaussian3d" << endl;
-//     int h, w, c;
-//     int padding = (kernel_size - 1) / 2;
-//     if (voxel.img_num > 0) {
-//         FImage temp(voxel.files[0]);
-//         h = temp.height; w = temp.width; c = temp.channel;
-//     } else {
-//         cout << "This Volume do not contain any images" << endl;
-//         return voxel;
-//     }
-//     voxel.preload();
-//     cout << "preload complete" << endl;
-
-//     for (int x = padding; x < voxel.img_num - padding; x++) {
-//         FImage res(h-(padding*2), (w-padding*2), c);
-//         for (int i = 0; i < res.height; i++) {
-//             for (int j = 0; j < res.width; j++) {
-//                 for (int k = 0; k < res.channel; k++) {
-//                     vector<uint8_t> temp;
-//                     for (int l = i; l < i+kernel_size; l++) {
-//                         for (int m = j; m < j+kernel_size; m++) {
-//                             for (int n = x-padding; n <= x+padding; n++) {
-//                                 temp.push_back(voxel.data[n].getPixel(l, m, k));
-//                             }
-//                         }
-//                     }
-//                     sort(temp.begin(), temp.end());
-//                     res.setPixel(i, j, k, temp[temp.size()-1]);
-//                 }
-//             }
-//         }
-//         cout << "Fimage constructed" << endl;
-//     }
-
-//     voxel.unload();
-//     return voxel;
-// }
