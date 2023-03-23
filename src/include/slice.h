25f01fe80c49afb5b48078b75395ad7a92819394
edsml-bd12559d <lizzie.withers22@imperial.ac.uk>
2023-03-23 14:07:29 +0000

    added header to every code file

--- src/include/slice.h
+++ src/include/slice.h
@@ -1,3 +1,11 @@
+/* Group: Monte Carlo
+Members: Zepeng Chen    | acse-zc522
+         Ruichen Ding   | acse-rd422
+         Georgie Mercer | edsml-gm1722
+         Chunyang Wang  | acse-cw17222
+         Lizzie Withers | edsml-lcw22
+*/
+
 # pragma once
 # include <string>
 # include "fastImage.h"
